ab681e37d947417a737681156736044baf0b8f1a
Taku <23535528+taku-shimomura@users.noreply.github.com>
2024-09-22 13:23:42 +0900

    [DONE] quiz2

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -17,17 +17,33 @@
 //   the first element is the string, the second one is the command.
 // - The output element is going to be a vector of strings.
 
-enum Command {
+pub enum Command {
     Uppercase,
     Trim,
     Append(usize),
 }
 
-mod my_module {
+pub mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut v: Vec<String> = Vec::new();
+        for tuple in input {
+            match tuple.1 {
+                Command::Uppercase => {
+                    v.push(tuple.0.to_uppercase());
+                },
+                Command::Trim => {
+                    v.push(tuple.0.trim().to_string());
+                },
+                Command::Append(num) => {
+                    v.push(format!("{}{}", tuple.0, "bar".repeat(num)));
+                }
+            }
+        }
+        v
+    }
 }
 
 fn main() {
@@ -37,7 +53,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
